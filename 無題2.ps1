ae6ef3ecb561b73bd65b2c13140b2061af9749a4
ysk76 <h.ysk.46@gmail.com>
2025-04-04 11:40:19 +0900

    Add files via upload

---
+++ 無題2.ps1
@@ -0,0 +1,34 @@
+﻿Add-Type @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+public class Reader
+{
+    public static List<List<string>> ReadCsv(string path)
+    {
+        List<List<string>> list = new List<List<string>>();
+        using (var reader = new StreamReader(path, Encoding.GetEncoding("shift_jis")))
+        {
+            while (!reader.EndOfStream)
+            {
+                List<string> csv = new List<string>();
+                string line = reader.ReadLine();
+                string[] values = line.Split(',');
+                foreach (string val in values)
+                {
+                    csv.Add(val);
+                }
+                list.Add(csv);
+            }
+
+        }
+        return list;
+    }
+}
+"@
+
+$csvList = [Test]::Exec("ファイルパス")
